f61279ed1b78b02d2ec039261cb43c66f9bd4414
Peter Denev <peter.denev@gmail.com>
2013-04-10 09:22:51 +0300

    0.0.11 release

--- Organel_1.php
+++ Organel_1.php
@@ -1,11 +1,10 @@
 <?php
 require_once './lib/Organel.php';
-require_once './lib/Chemical.php';
 
 class Organel_1 extends Organel{
 	function __construct($plasma, $config, $parent){
 		parent::__construct($plasma, $config, $parent);
-		if(isset($config['emit'])){
+		if(isset($config['emit']) && $config['emit']){
 			$this->emit(array('type'=>'test'));
 		}else{
 			$plasma->on('test',function(){
---
+++ README.md
@@ -0,0 +1,13 @@
+php-organic
+============
+
+Organic development in php
+(require PHP>=5.3)
+
+Based on node-organic
+=====
+https://github.com/outbounder/node-organic
+
+Wisdom
+=====
+http://wisdom.camplight.net/wisdom/50e85f3cb48c7a2213000002/organic-development-using-nodejs
