83c3120265349ab6e77e7f576e98e1c209ceba9a
Петър Денев (Peter Denev) <peter.denev@gmail.com>
2015-05-26 01:21:58 +0300

    clear old func tests

--- tests/ChemicalTest.php
+++ tests/ChemicalTest.php
@@ -13,18 +13,6 @@ class ChemicalTest extends PHPUnit_Framework_TestCase{
 		$this->assertTrue(gettype($c)=='object');
 	}
 
-	public function test_type_data_instance(){
-		$c = new Chemical('value', array("prop"=>"v"));		
-		$this->assertEquals('value', $c->type);		
-		$this->assertEquals("v", $c->data['prop']);						
-	}
-
-	public function test_json_string_data_instance(){
-		$c = new Chemical('{"type": "value", "data": {"prop": "v"}}');	
-		$this->assertEquals('value', $c->type);
-		$this->assertEquals("v", $c->data['prop']);
-	}
-
 	public function test_assoc_arr_data_instance(){
 		$c = new Chemical( array("type"=>"value",
 								 "data"=>array("prop"=>"v")) );	
